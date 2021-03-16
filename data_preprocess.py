adca03cc065f84d0f838fa35384a31164c9685d2
Jiang Xintong <50687608+NilesJiang@users.noreply.github.com>
2021-03-16 10:08:31 -0400

    Update data_preprocess.py

--- data_preprocess.py
+++ data_preprocess.py
@@ -3,15 +3,15 @@ import data_utils as d_util
 def __init__(self, path):
   self.path = path;
    # process terabyte data
-   d_util.getCriteoAdData(
-     #datafile="<path-to-day_{0,...,23}>",
-     datafile = "../dataset/day_4.gz"
-     o_filename=terabyte_processed.npz,
-     max_ind_range=-1,
-     sub_sample_rate=0.0,
-     days=24,
-     data_split='train',
-     randomize='total',
-     criteo_kaggle=False,
-     memory_map=False
+    d_util.getCriteoAdData(
+     #datafile="<path-to-day_{0,...,23}>""
+      datafile = "../dataset/day_4",
+      o_filename=terabyte_processed.npz,
+      max_ind_range=-1,
+      sub_sample_rate=0.0,
+      days=1,
+      data_split='train',
+      randomize='total',
+      criteo_kaggle=False,
+      memory_map=False
    )
