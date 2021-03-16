95c8f5ba03ccf44f406b463079bed190a8200b20
Jiang Xintong <50687608+NilesJiang@users.noreply.github.com>
2021-03-16 10:15:16 -0400

    Update data_preprocess.py

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,7 +1,6 @@
 import data_utils as d_util
 
-def __init__(self, path):
-  self.path = path;
+def __init__(self):
   d_util.getCriteoAdData(
      #datafile="<path-to-day_{0,...,23}>""
     datafile = "../dataset/day_4",
