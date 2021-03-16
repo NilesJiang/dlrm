2851687613a2c6d387b5c3a0b0c0d926e9c71c73
Jiang Xintong <50687608+NilesJiang@users.noreply.github.com>
2021-03-16 10:27:28 -0400

    Update dlrm_data_pytorch.py

--- dlrm_data_pytorch.py
+++ dlrm_data_pytorch.py
@@ -71,7 +71,7 @@ class CriteoDataset(Dataset):
             days = 7
             out_file = "kaggleAdDisplayChallenge_processed"
         elif dataset == "terabyte":
-            days = 24
+            days = 1
             out_file = "terabyte_processed"
         else:
             raise(ValueError("Data set option is not supported"))
