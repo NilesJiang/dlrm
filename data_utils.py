dca0a0c0d24f001cb90fb0447c0aeed4e64114e6
Jiang Xintong <50687608+NilesJiang@users.noreply.github.com>
2021-04-06 02:26:08 -0400

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -949,7 +949,7 @@ def getCriteoAdData(
             # 26 categorical features (an extra space indicates that feature is
             # missing and will be interpreted as 0).
             for i in range(days):
-                datafile_i = "../days/" + datafile + "_" + str(i)   + ".gz"
+                datafile_i = "./" + datafile + "_" + str(i)   + ".gz"
                 if path.exists(str(datafile_i)):
                     print("Reading data from path=%s" % (str(datafile_i)))
                     # file day_<number>
